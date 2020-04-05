811ce227d98d9c1fd4b94adff519689fd762d7ac
Matt Boran <mattboran@gmail.com>
2020-04-05 18:22:23 -0400

    Add debug command

--- api/hue.py
+++ api/hue.py
@@ -15,37 +15,46 @@ from api.exceptions import (UninitializedException,
 
 class HueApi:
 
-    # TODO: - Add register user
     def __init__(self, bridge_ip_address=None):
         if not bridge_ip_address:
-            try:
-                cached = open('.pickle', 'rb')
-                loaded = pickle.load(cached)
-                cached.close()
-                bridge_ip_address = loaded.get('bridge_ip_address')
-                user_name = loaded.get('user_name')
-            except FileNotFoundError:
-                raise UninitializedException
+            bridge_ip_address, user_name = self.load_existing()
         else:
-            url = f'http://{bridge_ip_address}/api'
-            payload = {'devicetype': 'hue_cli'}
-            response = re.post(url, json=payload)
-            response = response.json()[0]
-            error = response.get('error')
-            if error:
-                if error['type'] == 1:
-                    raise DevicetypeException
-                else:
-                    raise ButtonNotPressedException
-            user_name = response.get('success').get('username')
-            with open('.pickle', 'wb') as pickle_file:
-                cache = {
-                    'bridge_ip_address': bridge_ip_address,
-                    'user_name': user_name
-                }
-                pickle.dump(cache, pickle_file)
+            user_name = self.create_new_user(bridge_ip_address)
+        self.user_name = user_name
+        self.bridge_ip_address = bridge_ip_address
         self.base_url = f'http://{bridge_ip_address}/api/{user_name}/lights'
         self.lights = self.get_all_lights()
+
+    def load_existing(self):
+        try:
+            cached = open('.pickle', 'rb')
+            loaded = pickle.load(cached)
+            cached.close()
+            bridge_ip_address = loaded.get('bridge_ip_address')
+            user_name = loaded.get('user_name')
+        except FileNotFoundError:
+            raise UninitializedException
+        return bridge_ip_address, user_name
+
+    def create_new_user(self, bridge_ip_address):
+        url = f'http://{bridge_ip_address}/api'
+        payload = {'devicetype': 'hue_cli'}
+        response = re.post(url, json=payload)
+        response = response.json()[0]
+        error = response.get('error')
+        if error:
+            if error['type'] == 1:
+                raise DevicetypeException
+            else:
+                raise ButtonNotPressedException
+        user_name = response.get('success').get('username')
+        with open('.pickle', 'wb') as pickle_file:
+            cache = {
+                'bridge_ip_address': bridge_ip_address,
+                'user_name': user_name
+            }
+            pickle.dump(cache, pickle_file)
+        return user_name
 
     def get_all_lights(self):
         url = self.base_url
@@ -57,6 +66,11 @@ class HueApi:
             hue_light = HueLight(id, name, state, self.base_url)
             lights.append(hue_light)
         return lights
+
+    def print_debug_info(self):
+        print(f"Bridge IP address: {self.bridge_ip_address}")
+        print(f"Bridge API key (username): {self.user_name}")
+        print(f"API Base URL: {self.base_url}")
 
     def list_lights(self):
         for light in self.lights:
--- command_router.py
+++ command_router.py
@@ -1,42 +1,61 @@
 from api import HueApi
 
-COMMANDS = ['init', 'list', 'on', 'off', 'toggle', 'brightness', 'color']
+COMMANDS = [
+    'init',
+    'debug',
+    'list',
+    'on',
+    'off',
+    'toggle',
+    'brightness',
+    'color']
 
 class CommandRouter:
 
     def route_command(self, command, args):
         if command == 'init':
-            try:
-                self.api = HueApi(bridge_ip_address=args[0])
-            except Exception as e:
-                print(e.msg)
-                return
-        else:
-            self.api = HueApi()
-        if command == 'list':
-            self.api.list_lights()
+            self.handle_init()
+            return
+        self.api = HueApi()
+        if command in ['list', 'debug']:
+            self.handle_no_arg_command(command)
         elif command in ['on', 'off', 'toggle']:
             self.handle_no_value_command(command, args)
         elif command in ['brightness', 'color']:
             self.handle_command_with_value(command, args)
 
+    def handle_init(self, args):
+        try:
+            self.api = HueApi(bridge_ip_address=args[0])
+        except Exception as e:
+            print(f"Initialization error: {e.msg}")
+
+    def handle_no_arg_command(self, command):
+        command_map = {
+            'list': self.api.list_lights,
+            'debug': self.api.print_debug_info
+        }
+        action = command_map[command]
+        action()
+
     def handle_no_value_command(self, command, args):
-        if command == 'on':
-            action = self.api.turn_on
-        elif command == 'off':
-            action = self.api.turn_off
-        elif command == 'toggle':
-            action = self.api.toggle
+        command_map = {
+            'on': self.api.turn_on,
+            'off': self.api.turn_off,
+            'toggle': self.api.toggle
+        }
+        action = command_map[command]
         light = None
         if len(args) == 1:
             light = args[0]
         action(index=light)
 
     def handle_command_with_value(self, command, args):
-        if command == 'brightness':
-            action = self.api.set_brightness
-        elif command == 'color':
-            action = self.api.set_color
+        command_map = {
+            'brightness': self.api.set_brightness,
+            'color': self.api.set_color
+        }
+        action = command_map[command]
         light = None
         value = args[0]
         if len(args) == 2:
